822ab3799a755562b0792fe5fc770ec500b66fe0
me12fre <johnewoi72@gmail.com>
2024-09-20 17:00:41 +0300

    Hello world

--- app/src/main/java/com/example/forthkotlinproject/Hello-world.kt
+++ app/src/main/java/com/example/forthkotlinproject/Hello-world.kt
@@ -2,5 +2,5 @@ package com.example.forthkotlinproject
 
 fun main() {
     println("Hello world")// Print hello world
-    print("Hello world")// Print hello world
+    print("Helllearo world")// Print hello world
 }
